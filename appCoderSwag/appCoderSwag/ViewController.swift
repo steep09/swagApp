47804fc2c56e7072e4a28a55d45b2464dc4be8cd
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-09 10:23:09 +0800

    initial commit

---
+++ appCoderSwag/appCoderSwag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appCoderSwag
+//
+//  Created by Stephenson Ang on 09/05/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
